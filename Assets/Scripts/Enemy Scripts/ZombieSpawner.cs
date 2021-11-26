f11638b2da92c61319e79e0a2f2b19e8b895560a
Im-Sam <53315422+Im-Sam@users.noreply.github.com>
2021-11-26 12:33:05 +0000

    lighting + zombie spawn points

--- Assets/Scripts/Enemy Scripts/ZombieSpawner.cs
+++ Assets/Scripts/Enemy Scripts/ZombieSpawner.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ZombieSpawner : MonoBehaviour
 {
+    public GameObject[] spawner;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawner = new GameObject[20];
     }
 
     // Update is called once per frame
