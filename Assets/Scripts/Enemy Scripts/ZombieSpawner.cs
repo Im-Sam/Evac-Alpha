bcd5c8a579ee91b86140f539ac7f477fdcb32e48
Im-Sam <53315422+Im-Sam@users.noreply.github.com>
2021-11-26 14:10:45 +0000

    zombiespawner.cs

--- Assets/Scripts/Enemy Scripts/ZombieSpawner.cs
+++ Assets/Scripts/Enemy Scripts/ZombieSpawner.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class ZombieSpawner : MonoBehaviour
 {
     public GameObject[] spawner;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        spawner = new GameObject[20];
+        spawner = new GameObject[5];
+
+        for (int i = 0; i < spawner.Length; i++)
+        {
+            spawner[i] = transform.GetChild(i).gameObject;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnEnemy();
+        }
+    }
+    void SpawnEnemy()
+    {
+        int spawnerID = Random.Range(0, spawner.Length);
+        Instantiate(enemy, spawner[Random.Range(0, spawner.Length)].transform.position, spawner[Random.Range(0, spawner.Length)].transform.rotation);
     }
 }
