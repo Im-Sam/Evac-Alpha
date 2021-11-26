5110bace374ec67228788456d9b3ea3fce694d10
JosephDonnellan <b00137553@mytudublin.ie>
2021-11-26 19:36:51 +0000

    MuzzleFlash/Crosshair/Sound
    Muzzle flash, firing, sound of shot and crosshair added

--- Assets/LowPoly Environment Pack/Demo/Gun.cs
+++ Assets/LowPoly Environment Pack/Demo/Gun.cs
@@ -5,13 +5,19 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    AudioSource m_ShootingSound;
 
     public Camera fpsCam;
+
+    void Start(){
+        m_ShootingSound = GetComponent<AudioSource>();
+    }
     
     // Update is called once per frame
     void Update(){
         if (Input.GetButtonDown("Fire1")){
             Shoot();
+            m_ShootingSound.Play();
         }
         
     }
